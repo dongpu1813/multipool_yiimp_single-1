13996208249b756838cb084083fd60a006694abb
jhgsbgj <59393537+jhgsbgj@users.noreply.github.com>
2021-02-07 14:55:00 +0800

    Update questions.sh

--- questions.sh
+++ questions.sh
@@ -25,26 +25,8 @@ else
   \n\nAfter answering the following questions, setup will be automated.
   \n\nNOTE: If installing on a system with less then 8 GB of RAM you may experience system issues!"
 fi
-
-
-# Begin user inputted responses for auto install
-
-
-dialog --title "Using Domain Name" \
---yesno "Are you using a domain name? Example: example.com?
-Make sure the DNS is updated!" 7 60
-response=$?
-case $response in
-   0) UsingDomain=yes;;
-   1) UsingDomain=no;;
-   255) echo "[ESC] key pressed.";;
-esac
-
-if [[ ("$UsingDomain" == "yes") ]]; then
-
 dialog --title "Using Sub-Domain" \
---yesno "Are you using a sub-domain for the main website domain? Example pool.example.com?
-Make sure the DNS is updated!" 7 60
+--yesno "Are you using a sub-domain for the main website domain? Example pool.example.com? Make sure the DNS is updated!" 7 60
 response=$?
 case $response in
    0) UsingSubDomain=yes;;
@@ -52,85 +34,14 @@ case $response in
    255) echo "[ESC] key pressed.";;
 esac
 
-if [ -z "${DomainName}" ]; then
-DEFAULT_DomainName=example.com
-input_box "Domain Name" \
-"Enter your domain name. If using a subdomain enter the full domain as in pool.example.com
-\n\nDo not add www. to the domain name.
-\n\nMake sure the domain is pointed to this server before continuing!
-\n\nDomain Name:" \
-${DEFAULT_DomainName} \
-DomainName
-
-if [ -z "${DomainName}" ]; then
-# user hit ESC/cancel
-exit
-fi
-fi
-
-if [ -z "${StratumURL}" ]; then
-DEFAULT_StratumURL=stratum.${DomainName}
-input_box "Stratum URL" \
-"Enter your stratum URL. It is recommended to use another subdomain such as stratum.${DomainName}
-\n\nDo not add www. to the domain name.
-\n\nStratum URL:" \
-${DEFAULT_StratumURL} \
-StratumURL
-
-if [ -z "${StratumURL}" ]; then
-# user hit ESC/cancel
-exit
-fi
-fi
-
 dialog --title "Install SSL" \
---yesno "Would you like the system to install SSL automatically?" 7 60
+--yesno "Would you like the system to install SSL automatically? If just using the server IP you MUST select NO!" 7 60
 response=$?
 case $response in
    0) InstallSSL=yes;;
    1) InstallSSL=no;;
    255) echo "[ESC] key pressed.";;
 esac
-else
-
-# If user is not using a domain and is just using the server IP these fileds can be automatically detected.
-
-# Sets server IP automatically
-DomainName=$(get_publicip_from_web_service 4 || get_default_privateip 4)
-StratumURL=$(get_publicip_from_web_service 4 || get_default_privateip 4)
-UsingSubDomain=no
-InstallSSL=no
-fi
-
-# Back to user input questions regardless of domain name or IP use
-if [ -z "${SupportEmail}" ]; then
-DEFAULT_SupportEmail=root@localhost
-input_box "System Email" \
-"Enter an email address for the system to send alerts and other important messages.
-\n\nSystem Email:" \
-${DEFAULT_SupportEmail} \
-SupportEmail
-
-if [ -z "${SupportEmail}" ]; then
-# user hit ESC/cancel
-exit
-fi
-fi
-
-if [ -z "${AdminPanel}" ]; then
-DEFAULT_AdminPanel=AdminPortal
-input_box "Admin Panel Location" \
-"Enter your desired location name for admin access..
-\n\nOnce set you will access the YiiMP admin at ${DomainName}/site/AdminPortal
-\n\nDesired Admin Panel Location:" \
-${DEFAULT_AdminPanel} \
-AdminPanel
-
-if [ -z "${AdminPanel}" ]; then
-# user hit ESC/cancel
-exit
-fi
-fi
 
 dialog --title "Use AutoExchange" \
 --yesno "Would you like the stratum to be built with autoexchange enabled?" 7 60
@@ -150,6 +61,39 @@ case $response in
    255) echo "[ESC] key pressed.";;
 esac
 
+if [ -z "${DomainName}" ]; then
+DEFAULT_DomainName=$(get_publicip_from_web_service 4 || get_default_privateip 4)
+input_box "Domain Name" \
+"Enter your domain name. If using a subdomain enter the full domain as in pool.example.com
+\n\nDo not add www. to the domain name.
+\n\nMake sure the domain is pointed to this server before continuing!
+\n\nIf only using the server IP, enter the IP only!
+\n\nDomain Name:" \
+${DEFAULT_DomainName} \
+DomainName
+
+if [ -z "${DomainName}" ]; then
+# user hit ESC/cancel
+exit
+fi
+fi
+
+if [ -z "${StratumURL}" ]; then
+DEFAULT_StratumURL=${DomainName}
+input_box "Stratum URL" \
+"Enter your stratum URL. It is recommended to use another subdomain such as stratum.${DomainName}
+\n\nDo not add www. to the domain name.
+\n\nIf only using the server IP, enter the IP only!
+\n\nStratum URL:" \
+${DEFAULT_StratumURL} \
+StratumURL
+
+if [ -z "${StratumURL}" ]; then
+# user hit ESC/cancel
+exit
+fi
+fi
+
 if [ -z "${PublicIP}" ]; then
 DEFAULT_PublicIP=$(echo $SSH_CLIENT | awk '{ print $1}')
 input_box "Your Public IP" \
@@ -166,7 +110,6 @@ exit
 fi
 fi
 
-# These are all autgenerated but give user the oppertunity to set
 if [ -z "${DBRootPassword}" ]; then
 DEFAULT_DBRootPassword=$(openssl rand -base64 29 | tr -d "=+/")
 input_box "Database Root Password" \
@@ -212,21 +155,33 @@ exit
 fi
 fi
 
-clear
+if [ -z "${AdminPanel}" ]; then
+DEFAULT_AdminPanel=AdminPortal
+input_box "Admin Panel Location" \
+"Enter your desired location name for admin access..
+\n\nOnce set you will access the YiiMP admin at ${DomainName}/site/AdminPortal
+\n\nDesired Admin Panel Location:" \
+${DEFAULT_AdminPanel} \
+AdminPanel
 
+if [ -z "${AdminPanel}" ]; then
+# user hit ESC/cancel
+exit
+fi
+fi
+
+clear
 
 dialog --title "Verify Your Responses" \
 --yesno "Please verify your answers to continue setup:
 Dedicated Coin Ports : ${CoinPort}
 AutoExchange : ${AutoExchange}
-Using Domain : ${UsingDomain}
 Using Sub-Domain : ${UsingSubDomain}
 Install SSL      : ${InstallSSL}
 Domain Name      : ${DomainName}
 Stratum URL      : ${StratumURL}
-System Email     : ${SupportEmail}
 Your Public IP   : ${PublicIP}
-Admin Location   : ${AdminPanel}" 16 60
+Admin Location   : ${AdminPanel}" 15 60
 
 
 # Get exit status
